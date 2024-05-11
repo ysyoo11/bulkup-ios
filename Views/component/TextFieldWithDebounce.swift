f97fc95ca48a0172b5e5d305c8318a120bac8255
Yeonsuk Yoo <yysuk0302@gmail.com>
2024-05-11 22:15:19 +1000

    feat: Create TextFieldWithDebounce component

---
+++ Views/component/TextFieldWithDebounce.swift
@@ -0,0 +1,18 @@
+//
+//  TextFieldWithDebounce.swift
+//  BulkUp
+//
+//  Created by Yeonsuk Yoo on 11/5/2024.
+//
+
+import SwiftUI
+
+struct TextFieldWithDebounce: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextFieldWithDebounce()
+}
